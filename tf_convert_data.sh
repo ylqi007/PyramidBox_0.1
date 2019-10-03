08559db5dd3a0e5219b54a721a4f8dee5b875932
ylqi007 <yunlongqi@my.unt.edu>
2019-10-03 14:41:06 -0500

    20191003, add tf_convert_data

---
+++ tf_convert_data.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+VOC2007_TRAIN="../../DATA/VOC2007/train/"
+VOC2007_TEST="../../DATA/VOC2007/test/"
+
+DATA_OUTPUT="/tmp/pascalvoc_tfrecord/"
+
+if [[ ! -d ${DATA_OUTPUT} ]]; then
+  mkdir ${DATA_OUTPUT}
+fi
+
+# Convert Pascal VOC train to tfrecord files.
+python tf_convert_data.py \
+    --dataset_name=pascalvoc \
+    --dataset_dir=${VOC2007_TRAIN} \
+    --output_name=voc_2007_train \
+    --output_dir=${DATA_OUTPUT}
+
+# Convert Pascal VOC train to tfrecord files.
+python tf_convert_data.py \
+    --dataset_name=pascalvoc \
+    --dataset_dir=${VOC2007_TEST} \
+    --output_name=voc_2007_test \
+    --output_dir=${DATA_OUTPUT}
